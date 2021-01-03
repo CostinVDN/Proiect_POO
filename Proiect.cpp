e782e19de063e44b9cb161e2abb63491ac2f072f
Luiza <luiza.tudorache@gmail.com>
2021-01-03 21:31:22 +0200

    Update Proiect.cpp

--- Proiect.cpp
+++ Proiect.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
 using namespace std;
 
 class Film
@@ -28,7 +28,7 @@ public:
 		pozitie_top = 0;
 	}
 
-	Film(char* nume, int* frecventa, string gen, int durata, int pozitie_top=20) :idFilm(++nrFilme)
+	Film(char* nume, int* frecventa, string gen, int durata, int pozitie_top = 20) :idFilm(++nrFilme)
 	{
 		if (strlen(nume) != 0)
 		{
@@ -305,7 +305,7 @@ public:
 	Film operator>=(const Film& f)
 	{
 
-		int nr_zile_film1=0, nr_zile_film2=0;
+		int nr_zile_film1 = 0, nr_zile_film2 = 0;
 
 		for (int indx = 0; indx < 7; indx++)
 		{
@@ -323,11 +323,11 @@ public:
 
 		if (nr_zile_film1 < nr_zile_film2)
 			return f;
-		
+
 	}
 
 	//Verifica daca doua filme ruleaza in aceleasi zile
-	bool operator==(Film &f)
+	bool operator==(Film& f)
 	{
 
 		for (int indx = 0; indx < 7; indx++)
@@ -435,6 +435,190 @@ istream& operator>> (istream& in, Film& f)
 	return in;
 }
 
+
+class Client
+{
+private:
+	const int idClient;
+	static int nrClienti;
+	char* nume;
+	int nrCarduri;
+	int* dateCard;
+	string email;
+
+public:
+	Client() : idClient(++nrClienti)
+	{
+		nume = nullptr;
+		nrCarduri = 0;
+		dateCard = nullptr;
+		email = "";
+	}
+
+	Client(char* nume, int* dateCard, int nrCarduri, string tipClient, bool abonat) : idClient(++nrClienti)
+	{
+		if (nume != nullptr)
+		{
+			this->nume = new char[strlen(nume) + 1];
+			strcpy_s(this->nume, strlen(nume) + 1, nume);
+		}
+		else
+		{
+			this->nume = nullptr;
+		}
+
+		if (dateCard != nullptr && nrCarduri > 0)
+		{
+			this->dateCard = new int[nrCarduri];
+			for (int i = 0; i < nrCarduri; i++)
+			{
+				this->dateCard[i] = dateCard[i];
+			}
+			this->nrCarduri = nrCarduri;
+		}
+		else
+		{
+			this->dateCard = nullptr;
+			this->nrCarduri = 0;
+		}
+
+		this->email = email;
+	}
+
+	Client(const Client& c) : idClient(c.idClient)
+	{
+		if (c.nume != nullptr)
+		{
+			nume = new char[strlen(c.nume) + 1];
+			strcpy_s(nume, strlen(c.nume) + 1, c.nume);
+		}
+		else
+		{
+			nume = nullptr;
+		}
+
+		if (c.dateCard != nullptr && c.nrCarduri > 0)
+		{
+			dateCard = new int[c.nrCarduri];
+			for (int i = 0; i < c.nrCarduri; i++)
+			{
+				dateCard[i] = c.dateCard[i];
+			}
+			nrCarduri = c.nrCarduri;
+		}
+		else
+		{
+			dateCard = nullptr;
+			nrCarduri = 0;
+		}
+
+		email = c.email;
+	}
+
+	~Client()
+	{
+		delete[] nume;
+		delete[] dateCard;
+
+	}
+
+	Client& operator=(Client& c)
+	{
+		delete[] nume;
+		delete[] dateCard;
+
+		if (c.nume != nullptr)
+		{
+			nume = new char[strlen(c.nume) + 1];
+			strcpy_s(nume, strlen(c.nume) + 1, c.nume);
+		}
+		else
+		{
+			nume = nullptr;
+		}
+
+		if (c.dateCard != nullptr && c.nrCarduri > 0)
+		{
+			dateCard = new int[c.nrCarduri];
+			for (int i = 0; i < c.nrCarduri; i++)
+			{
+				dateCard[i] = c.dateCard[i];
+			}
+			nrCarduri = c.nrCarduri;
+		}
+		else
+		{
+			dateCard = nullptr;
+			nrCarduri = 0;
+		}
+
+		email = c.email;
+
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream&, Client);
+	friend istream& operator>>(istream&, Client&);
+};
+
+int Client::nrClienti = 0;
+
+ostream& operator<<(ostream& out, Client c)
+{
+	if (c.nume != nullptr)
+	{
+		out << "Nume: " << c.nume << endl;
+	}
+	out << "Numar carduri inregistrate: " << c.nrCarduri << endl;
+	if (c.dateCard != nullptr)
+	{
+		for (int i = 0; i < c.nrCarduri; i++)
+		{
+			out << "Numar card " << i + 1 << ": " << c.dateCard[i] << endl;
+		}
+	}
+	out << "Email: " << c.email << endl;
+	return out;
+}
+
+istream& operator>>(istream& in, Client& c)
+{
+	cout << "Nume: ";
+	delete[] c.nume;
+	char buffer[50];
+	//in >> ws;
+	in.getline(buffer, 49);
+	c.nume = new char[strlen(buffer) + 1];
+	strcpy_s(c.nume, strlen(buffer) + 1, buffer);
+
+	if (c.dateCard != nullptr)
+	{
+		delete[] c.dateCard;
+	}
+	cout << endl << "Numar carduri: ";
+	in >> c.nrCarduri;
+	if (c.nrCarduri > 0)
+	{
+		c.dateCard = new int[c.nrCarduri];
+		for (int i = 0; i < c.nrCarduri; i++)
+		{
+			cout << endl << "Numar card " << i + 1 << ": ";
+			in >> c.dateCard[i];
+		}
+	}
+	else
+	{
+		c.nrCarduri = 0;
+		c.dateCard = nullptr;
+	}
+	cout << endl << "Email: ";
+	in >> ws;
+	getline(in, c.email);
+	cout << endl;
+
+	return in;
+}
+
 int main()
 {
 
@@ -444,19 +628,19 @@ int main()
 	cin >> film_nou;
 	cout << film_nou;*/
 
-	int zile[] = {0, 1, 1, 0, 0, 1, 0 };
-	Film film_nou_1((char*) "Film 1", zile, "Comedie", 90,15);
+	int zile[] = { 0, 1, 1, 0, 0, 1, 0 };
+	Film film_nou_1((char*)"Film 1", zile, "Comedie", 90, 15);
 
-	int zile1[] = {1, 1, 1, 0, 0, 1, 1 };
-	Film film_nou_2((char*)"Film 2", zile1, "Comedie", 85,11);
+	int zile1[] = { 1, 1, 1, 0, 0, 1, 1 };
+	Film film_nou_2((char*)"Film 2", zile1, "Comedie", 85, 11);
 
-	int zile2[] = {1, 0, 1, 0, 0, 1, 1 };
-	Film film_nou_3((char*) "Film 3", zile2, "Actiune", 120,12);
+	int zile2[] = { 1, 0, 1, 0, 0, 1, 1 };
+	Film film_nou_3((char*)"Film 3", zile2, "Actiune", 120, 12);
 
 	Film filme[] = { film_nou_1, film_nou_2, film_nou_3 };
 
 	cout << filme[1] << endl;
-	cout << filme[2] << endl <<endl;
+	cout << filme[2] << endl << endl;
 
 	if (filme[1] == filme[2])
 	{
@@ -493,3 +677,4 @@ int main()
 
 	cout << filme[2]++;*/
 }
+
